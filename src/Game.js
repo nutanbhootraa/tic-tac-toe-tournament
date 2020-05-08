bc2510432041e7f36389c24d61031196facad93a
mystery <bhootra1993@gmail.com>
2020-05-08 17:49:01 +0530

    TicTacToe tournament

--- src/Game.js
+++ src/Game.js
@@ -4,8 +4,8 @@ import Board from "./Board";
 class Game extends React.Component {
     render() {
         return (
-        <div>
-            <div className="game-board">
+        <div className="game">
+            <div>
                 <Board player1={this.props.player1} player2={this.props.player2}/>
             </div>
         </div>
